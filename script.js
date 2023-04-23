11549566da9871d88aeb13d7546bcf8c607eaf3d
Nick <nprice0502@yahoo.com>
2023-04-23 17:11:54 -0400

    added some content to the quizquestions variable

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //create an array of questions for the user to answer with a list of choices
 var quizQuestions = [
-
-]
+{
+    question: "Which file do you create styling in?",
+    choices: ["html","css","Javascript"],
+    answer: "css"
+},
+{
+    question: "What is a programmers  best friend?",
+    choices: ["A keyboard","the mouse","google","A Mentor"],
+    answer: "google"
+},
+];
