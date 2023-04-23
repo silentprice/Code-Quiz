c60d4c81f6a0b2760e98051dfc143bb35a18a8e3
Nick <nprice0502@yahoo.com>
2023-04-23 18:18:27 -0400

    added content into the displayQ function

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-//Create a function so when the user clicks the start button the timmer starts for the quiz
-function startQuiz() {
-    scoreContainerEl.style.display = "none";
-    quizContainerEl.style.display = "block";  
-}
-// create function so timer starts when i clock the start quiz button.
-function startTimer(){
-    timerEl.textContent= time;
-    
-}
-const startingTime = 60;
-let time = startingTime;
-let timerIntervalId = null;
-
 //create an array of questions for the user to answer with a list of choices
 var quizQuestions = [
 {
@@ -31,8 +17,6 @@ var quizQuestions = [
 }
 ];
 
-//Then the user is presented with another question
-
 //Elements form html to be updated during the game
 var quizContainerEl = document.getElementById("quiz-container");
 var questionEl = document.getElementById("question");
@@ -40,3 +24,40 @@ var choicesEl = document.getElementById("choices");
 var scoreEl = document.getElementById("score");
 var responseEl = document.getElementById("response");
 var initialsEl = document.getElementById("initials");
+
+//Create a function so when the user clicks the start button the timmer starts for the quiz
+function startQuiz() {
+    scoreContainerEl.style.display = "none";
+    quizContainerEl.style.display = "block";  
+    displayQuestion();
+    startTimer();
+}
+
+//make a function to display the questionss and update the choices with current question
+function displayQuestion (){
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionEl.textContent = currentQuestion.question;
+    choicesEl.innerHTML = "";
+    for (let i = 0; i< currentQuestion.choices.length; i++){
+        var choice = currentQuestion.choices[i];
+        var choiceButton = document.createElement("button");
+        choiceButton.textContent = choice;
+        choiceButton.addEventListener("click", handleChoiceClick);
+        choicesEl.appendChild(choiceButton);
+    }
+}
+//make a function to hangle a choice click event
+
+// create function so timer starts when i clock the start quiz button.
+function startTimer(){
+    timerEl.textContent= time;
+    
+}
+const startingTime = 60;
+let time = startingTime;
+
+
+// Make a variable to keep track of users score
+let currentQuestionIndex=0;
+let score=0;
+//Then the user is presented with another question
