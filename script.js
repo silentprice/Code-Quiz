b7707af5492c633c156dbafeee2805d41c21a047
Nick <nprice0502@yahoo.com>
2023-04-23 21:51:31 -0400

    updated some of the functions for start quiz

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//define a list of variables
+var currentQuestionIndex = 0;
+var time = 60;
+var score = 0;
+var timerInterval;
 //create an array of questions for the user to answer with a list of choices
 var quizQuestions = [
 {
@@ -17,6 +22,7 @@ var quizQuestions = [
 }
 ];
 
+
 //Elements form html to be updated during the game
 var quizContainerEl = document.getElementById("quiz-container");
 var questionEl = document.getElementById("question");
@@ -26,12 +32,19 @@ var responseEl = document.getElementById("response");
 var initialsEl = document.getElementById("initials");
 
 //Create a function so when the user clicks the start button the timmer starts for the quiz
-function startQuiz(button) {
-    scoreContainerEl.style.display = "none";
+function startQuiz() {
+    var startButtonEl = document.getElementById("start-button");
+    startButtonEl.style.display="none";
+   //have the quiz container pop up once start is clicked
+   var quizContainerEl = document.getElementById("quiz-container");
     quizContainerEl.style.display = "block";  
-    displayQuestion();
-    startTimer();
+    //have the timeer start for the quiz
+    timerInterval = setInterval(function(){
+        time--;
+        var timeEl = document.getElementById("time");
+    }) 
 }
+
 
 //make a function to display the questionss and update the choices with current question
 function displayQuestion (){
@@ -48,8 +61,8 @@ function displayQuestion (){
 }
 //make a function to hangle a choice click event
 function handleChoiceClick(event) {
-    const choice = event.target.textContent;
-    const currentQuestion = quizQuestions[currentQuestionIndex];
+    var choice = event.target.textContent;
+    var currentQuestion = quizQuestions[currentQuestionIndex];
 if (choice === currentQuestion.answer){
     responseEl.textContent = "Correct!";
     score += 10;
@@ -76,15 +89,11 @@ function startTimer(){
             endQuiz();
         } 
     }, 60000) }
-var startingTime = 60;
-let time = startingTime;
+
 
 //create a function to end the quiz
 function endQuiz(){
     clearInterval(timerInterValid);
     quizContainerEl.style.dislpay="none";
-    scoreContainerEl;
 }
-// Make a variable to keep track of users score
-let currentQuestionIndex=0;
-let score=0;
+
